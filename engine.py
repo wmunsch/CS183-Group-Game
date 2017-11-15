50e9ed345b92cc1587069a5678edbacb47688317
wmunsch <31708427+wmunsch@users.noreply.github.com>
2017-11-15 13:23:51 -0500

    updated

--- engine.py
+++ engine.py
@@ -6,14 +6,27 @@ screen = pygame.display.set_mode((1280, 700))
 done = False
 x=640
 y=350
-theChar = pygame.image.load('graphics/character.png')
+
+
+
+theChar = pygame.image.load('desktop/graphics/character.png')
 theChar = pygame.transform.scale(theChar,(76,112))
-room1 = pygame.image.load('graphics/room1.png')
-room1 = room1.convert()
-room1 = pygame.transform.scale(room1,(1280,720))
+floortile = pygame.image.load('desktop/graphics/floortile.png')
+floortile = pygame.transform.scale(floortile,(100,100))
+
+#room1 = pygame.image.load('graphics/room1.png')
+#room1 = room1.convert()
+#room1 = pygame.transform.scale(room1,(1280,720))
 framerate = pygame.time.Clock()
 
+#def createRoom():
+    
+class Player:
+    x = 640
+    y = 350
+    health = 10
 
+player = Player()
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -21,15 +34,20 @@ while not done:
 
         ##basic controls
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 4
-        if pressed[pygame.K_DOWN]: y+= 4
-        if pressed[pygame.K_LEFT]: x -= 4
-        if pressed[pygame.K_RIGHT]: x+= 4
+        
+        
+        if pressed[pygame.K_UP]: 
+            if player.y >= 400:player.y -= 4
+        if pressed[pygame.K_DOWN]: player.y+= 4
+        if pressed[pygame.K_LEFT]: player.x -= 4
+        if pressed[pygame.K_RIGHT]: player.x+= 4
 
+            
         screen.fill((0,0,0))
-        screen.blit(room1,(0,0))
-
-        screen.blit(theChar,(x,y))
+        #screen.blit(room1,(0,0))
+        screen.blit(floortile,(100,100)) 
+        screen.blit(floortile,(200,200))
+        screen.blit(theChar,(player.x,player.y))
 
         #surface = pygame.image.load('graphics/character.png')
         pygame.display.flip()
