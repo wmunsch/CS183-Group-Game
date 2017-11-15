c64188ec72815cd99769253cf1f5341fa8b39dd2
wmunsch <31708427+wmunsch@users.noreply.github.com>
2017-11-15 13:42:44 -0500

    updated engine

--- engine.py
+++ engine.py
@@ -14,7 +14,7 @@ theChar = pygame.transform.scale(theChar,(76,112))
 floortile = pygame.image.load('desktop/graphics/floortile.png')
 floortile = pygame.transform.scale(floortile,(100,100))
 
-#room1 = pygame.image.load('graphics/room1.png')
+#room1 = pygame.image.load('desktop/graphics/room1.png')
 #room1 = room1.convert()
 #room1 = pygame.transform.scale(room1,(1280,720))
 framerate = pygame.time.Clock()
@@ -25,8 +25,24 @@ class Player:
     x = 640
     y = 350
     health = 10
+    
+class Goblin:
+    x = 0
+    y = 0
+    health = 3
+    
+class room1:
+    roomimage = pygame.image.load('desktop/graphics/room1.png')
+    roomimage = roomimage.convert()
+    roomimage = pygame.transform.scale(roomimage,(1280,720))
+    wallup = 60
+    walldown = 500
+    wallleft = 100
+    wallright = 1100
+    
 
 player = Player()
+firstroom = room1()
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -37,14 +53,17 @@ while not done:
         
         
         if pressed[pygame.K_UP]: 
-            if player.y >= 400:player.y -= 4
-        if pressed[pygame.K_DOWN]: player.y+= 4
-        if pressed[pygame.K_LEFT]: player.x -= 4
-        if pressed[pygame.K_RIGHT]: player.x+= 4
+            if player.y >= firstroom.wallup:player.y -= 4
+        if pressed[pygame.K_DOWN]: 
+            if player.y <= firstroom.walldown:player.y+= 4
+        if pressed[pygame.K_LEFT]: 
+            if player.x >= firstroom.wallleft:player.x -= 4
+        if pressed[pygame.K_RIGHT]:
+            if player.x <= firstroom.wallright:player.x+= 4
 
             
         screen.fill((0,0,0))
-        #screen.blit(room1,(0,0))
+        screen.blit(firstroom.roomimage,(0,0))
         screen.blit(floortile,(100,100)) 
         screen.blit(floortile,(200,200))
         screen.blit(theChar,(player.x,player.y))
