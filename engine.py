21aee8955d399d21a5a2a45467b40c328b2e2dc0
wmunsch <31708427+wmunsch@users.noreply.github.com>
2017-11-18 17:56:14 -0500

    finished walking anim

--- engine.py
+++ engine.py
@@ -6,47 +6,108 @@ screen = pygame.display.set_mode((1280, 700))
 done = False
 framerate = pygame.time.Clock()
 
-    
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.image.load('graphics/character.png')
         self.surf = pygame.transform.scale(self.surf,(76,112))
+        self.animation_speed = 10
+        self.animation_speed = self.animation_speed
         self.rect = self.surf.get_rect()
         self.rect.move_ip(400,0)
-        
+        self.walk_up_ani = [pygame.transform.scale(pygame.image.load('graphics/player_walking_upF1.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_upF2.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_upF3.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_upF4.png'),(92,124))]
+        self.walk_left_ani = [pygame.transform.scale(pygame.image.load('graphics/player_walking_leftF1.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_leftF2.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_leftF3.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_leftF4.png'),(92,124))]
+        self.walk_right_ani = [pygame.transform.scale(pygame.image.load('graphics/player_walking_rightF1.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_rightF2.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_rightF3.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_rightF4.png'),(92,124))]
+        self.walk_down_ani = [pygame.transform.scale(pygame.image.load('graphics/player_walking_downF1.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_downF2.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_downF3.png'),(92,124)),
+            pygame.transform.scale(pygame.image.load('graphics/player_walking_downF4.png'),(92,124))]
+
+        #self.noi = 4
+        self.current_frame = 0
+        self.time = 0
+
+
     def update(self, pressed_keys):
+######### walking up animation ##############
         if pressed_keys[pygame.K_UP]:
             self.rect.move_ip(0, -self.speed)
-        if pressed_keys[pygame.K_DOWN]:
+            self.time += 1
+            if (self.time % 12 == 0):
+                if(self.current_frame>2):
+                    self.current_frame = 0
+                else:
+                    self.current_frame+=1
+
+            self.surf = self.walk_up_ani[self.current_frame]
+
+######### walking down animation ##############
+        elif pressed_keys[pygame.K_DOWN]:
             self.rect.move_ip(0, self.speed)
-        if pressed_keys[pygame.K_LEFT]:
+            self.time += 1
+            if (self.time % 12 == 0):
+                if(self.current_frame>2):
+                    self.current_frame = 0
+                else:
+                    self.current_frame+=1
+
+            self.surf = self.walk_down_ani[self.current_frame]
+
+######### walking left animation ##############
+        elif pressed_keys[pygame.K_LEFT]:
             self.rect.move_ip(-self.speed, 0)
-        if pressed_keys[pygame.K_RIGHT]:
+            self.time += 1
+            if (self.time % 12 == 0):
+                if(self.current_frame>2):
+                    self.current_frame = 0
+                else:
+                    self.current_frame+=1
+
+            self.surf = self.walk_left_ani[self.current_frame]
+
+######### walking right animation ##############
+        elif pressed_keys[pygame.K_RIGHT]:
             self.rect.move_ip(self.speed, 0)
-            
-            
-          
-    speed = 5
+            self.time += 1
+            if (self.time % 12 == 0):
+                if(self.current_frame>2):
+                    self.current_frame = 0
+                else:
+                    self.current_frame+=1
+
+            self.surf = self.walk_right_ani[self.current_frame]
+
+
+    speed = 4
     x = 640
     y = 350
     health = 10
-    
-    
 
-    
-    
+
+
+
+
 class Goblin:
     x = 0
     y = 0
     health = 3
-    
-    
+
+
 class Wall(pygame.sprite.Sprite):
     def __init__(self,x,y,width,height):
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect(x,y,width,height)
-    
+
 class room1:
     roomimage = pygame.image.load('graphics/room2.png')
     roomimage = roomimage.convert()
@@ -60,25 +121,25 @@ class room1:
     wallBottom1 = Wall(0,610,1280,50)
     wallBottom2 = Wall(0,0,0,0)
     wallMiddle = Wall(200,0,200,300)
-    
-        
-    
+
+
+
     #wallup = 60
     #walldown = 500
     #wallleft = 100
     #wallright = 1100
-    
-    
+
+
 #use this to update the current room when changing rooms
 #def updateCurrentRoom(room):
 #    currentroom = room
-    
-    
+
+
 def checkCollision(self, sprite1, sprite2):
     col = pygame.sprite.collide_rect(sprite1, sprite2)
     return(col)
-        
-    
+
+
 player = Player()
 firstroom = room1()
 currentroom = firstroom
@@ -87,6 +148,7 @@ sprites_walls = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 ## add sprites to this group to draw them to the screen
 sprites_alive.add(player)
+
 sprites_walls.add(firstroom.wallLeft1)
 all_sprites.add(sprites_alive,sprites_walls)
 
@@ -96,11 +158,11 @@ while not done:
                         done = True
 
 
-        pressed_keys = pygame.key.get_pressed()  
-   
+        pressed_keys = pygame.key.get_pressed()
+
         #if (player.rect.left < currentmap.wallLeft.right):
            #player.rect.move_ip(5, 0)
-        
+
         ##using this method of collision i dont know how to make walls in the middle of the screen
         if (checkCollision(pygame.sprite.Sprite, player,currentroom.wallLeft1) or checkCollision(pygame.sprite.Sprite, player,currentroom.wallLeft2)):
             player.rect.move_ip(5, 0)
@@ -110,19 +172,19 @@ while not done:
             player.rect.move_ip(0, 5)
         if (checkCollision(pygame.sprite.Sprite, player,currentroom.wallBottom1) or checkCollision(pygame.sprite.Sprite, player,currentroom.wallBottom2)):
             player.rect.move_ip(0, -5)
-        
+
         player.update(pressed_keys)
-        
+
         #screen.fill((0,0,0)) dont use anymore
         screen.blit(firstroom.roomimage,(0,0))
-       
+
         for entity in sprites_alive:
             screen.blit(entity.surf, entity.rect)
-           
+
         #use the following for collision detection between player and enemies
         #if pygame.sprite.spritecollideany(player, enemies):
             #player.kill()
-        
+
 
         pygame.display.flip()
         framerate.tick(60) #sets the framerate to 60 fps
